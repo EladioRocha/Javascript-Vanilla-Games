9a15d1a92d016ef56e54233000249febbeb974d5
Eladio Rocha Vizcaino <eladio.rocha99@gmail.com>
2020-09-07 01:03:04 -0500

    buscaminas

--- buscaminas/index.js
+++ buscaminas/index.js
@@ -5,7 +5,8 @@ const WIDTH = 10,
 
 const gameContainer = document.querySelector('.game-container')
 
-let activeGame = true
+let gameActive = true,
+  flags = 0
 
 function createBoard() {
   // Generamos bombas de forma aleatoria
@@ -20,13 +21,17 @@ function createBoard() {
     gameContainer.appendChild(square) 
     SQUARES.push(square)
 
-    square.addEventListener('click', handleCellClick)
+    square.addEventListener('click', event => handleCellClick(event.target))
+    square.addEventListener('contextmenu', event => {
+      event.preventDefault()
+      addFlag(event.target)
+    })
   }
 
   // Agregamos los números que nos muestran la cantidad de bombas
   for(let i = 0; i < BOARD_SIZE; i++) {
     let total = 0
-    const isInLeftSide = i % WIDTH === 0 // Esto nos ayuda a ver si esta en el principio del lado izquierdo. Por ejemplo: 0, 10, 20 etc.
+    const isInLeftSide = (i % WIDTH) === 0 // Esto nos ayuda a ver si esta en el principio del lado izquierdo. Por ejemplo: 0, 10, 20 etc.
     const isInRightSide = (i % WIDTH) === (WIDTH - 1) // Esta constante nos ayuda a ver si esta al final del lado derecho, ejemplo: 9, 19, 29 etc
     if(SQUARES[i].className === '') {
       if(i > 0 && !isInLeftSide && SQUARES[i - 1].classList.contains('bomb')) total++ // Izquierda
@@ -45,21 +50,107 @@ function createBoard() {
 
 createBoard()
 
-function handleCellClick(event) {
-  const square = event.target
-  if(!activeGame) return
+function handleCellClick(square) {
+  if(!gameActive) return
   if(square.classList.contains('checked')) return
   if(square.classList.contains('bomb')) {
-    alert('Ooops... hay una bomba justo ahí.')
-    activeGame = false
+    gameOver(square)
   } else {
     let total = square.getAttribute('data-total')
     if(total != 0) {
       square.className = 'checked'
       square.innerText = total
     } else {
+      checkNeighbordSquares(square, square.id)
       square.className = 'checked'
     }
     console.log(total)
+  }
+}
+
+function checkNeighbordSquares(square, currentId) {
+  const isInLeftSide = (currentId % WIDTH) === 0 // Esto nos ayuda a ver si esta en el principio del lado izquierdo. Por ejemplo: 0, 10, 20 etc.
+  const isInRightSide = (currentId % WIDTH) === (WIDTH - 1) // Esta constante nos ayuda a ver si esta al final del lado derecho, ejemplo: 9, 19, 29 etc
+
+  setTimeout(() => {
+    if(currentId > 0 && !isInLeftSide ) {
+      const newId = SQUARES[parseInt(currentId) - 1].id,
+        newSquare = document.getElementById(newId)
+        handleCellClick(newSquare)
+    }
+    if(currentId > 9 && !isInRightSide) {
+      const newId = SQUARES[parseInt(currentId) + 1 - WIDTH].id
+      newSquare = document.getElementById(newId)
+      handleCellClick(newSquare)
+    }
+    if(currentId > 10) {
+      const newId = SQUARES[parseInt(currentId) - WIDTH].id
+      newSquare = document.getElementById(newId)
+      handleCellClick(newSquare)
+    }
+    if(currentId > 11 && !isInLeftSide) {
+      const newId = SQUARES[parseInt(currentId) - 1 - WIDTH].id
+      newSquare = document.getElementById(newId)
+      handleCellClick(newSquare)    }
+    if(currentId < 98 && !isInRightSide) {
+      const newId = SQUARES[parseInt(currentId) + 1].id
+      newSquare = document.getElementById(newId)
+      handleCellClick(newSquare)
+    }
+    if(currentId < 90 && !isInLeftSide) {
+      const newId = SQUARES[parseInt(currentId) - 1 + WIDTH].id
+      newSquare = document.getElementById(newId)
+      handleCellClick(newSquare)
+    }
+    if(currentId < 88 && !isInRightSide) {
+      const newId = SQUARES[parseInt(currentId) + 1 + WIDTH].id
+      newSquare = document.getElementById(newId)
+      handleCellClick(newSquare)
+    }
+    if(currentId < 89) {
+      const newId = SQUARES[parseInt(currentId) + WIDTH].id
+      newSquare = document.getElementById(newId)
+      handleCellClick(newSquare)
+    }
+  }, 100)
+}
+
+function gameOver() {
+  alert('Ooops... hay una bomba justo ahí.')
+  gameActive = false
+  for(const square of SQUARES) {
+    if(square.classList.contains('bomb')) {
+      square.innerText = 'X'
+    }
+  }
+}
+
+function addFlag(square) {
+  console.log('jejejeje')
+  if(!gameActive) return
+  if(!square.classList.contains('checked') && (flags < BOMB_AMOUNT)) {
+    if(!square.classList.contains('flag')) {
+      square.classList.add('flag')
+      square.innerText = 'F'
+      flags++
+      handleFlags()
+    } else {
+      square.classList.remove('flag')
+      square.innerText = ''
+      flags--
+    }
+  }
+}
+
+function handleFlags() {
+  let matches = 0
+  for(const square of SQUARES) {
+    if(square.classList.contains('flag') && square.classList.contains('bomb')) {
+      matches++
+    }
+    if(matches === BOMB_AMOUNT) {
+      gameActive = false
+      alert('Eres el ganador!!!!')
+    }
   }
 }
